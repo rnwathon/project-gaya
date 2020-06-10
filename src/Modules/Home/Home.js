b98cd6a349d3fdd52a98ca0568f25f26e72f14e3
rnwathon <rakhmatnashrul@gmail.com>
2020-06-11 07:09:00 +0800

    Added Gradient Generator routes

--- src/Modules/Home/Home.js
+++ src/Modules/Home/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
+import * as Path from '../../Constant/path';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render(){
     return(
-      "Welcome Home"
+      <React.Fragment>
+        <h1>Project Gaya</h1>
+        <br />
+        <Link to={Path.GradientGenerator}>Gradient Generator/</Link>
+      </React.Fragment>
     )
   }
 }
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,8 +1,10 @@
 import * as Path from "../Constant/path";
 import Home from "../Modules/Home/Home";
+import GradientGenerator from "../Modules/GradientGenerator/GradientGenerator";
 
 let routes = [
-  { path: Path.Home, name: "Home", component: Home, exact: true }
+  { path: Path.Home, name: "Home", component: Home, exact: true },
+  { path: Path.GradientGenerator, name: "Gradient Generator", component: GradientGenerator, exact: true },
 ]
 
 export default routes
