fda622e17eecfefaf45905131b11393e163fcc93
Rakhmat Nashrul Wathon <rakhmat@mhcasia.com>
2020-06-11 00:40:26 +0800

    Created dynamic routes variable

---
+++ src/Routes/index.js
@@ -0,0 +1,8 @@
+import * as Path from "../Constant/path";
+import Home from "../Modules/Home/Home";
+
+let routes = [
+  { path: Path.Home, name: "Home", component: Home, exact: true }
+]
+
+export default routes
