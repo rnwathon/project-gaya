cbe6f1035902e20fdeb046e134992bc4f3c2a155
rnwathon <rakhmatnashrul@gmail.com>
2020-06-11 09:02:44 +0800

    Added randomizer for the gradients

--- src/Modules/GradientGenerator/GradientGenerator.js
+++ src/Modules/GradientGenerator/GradientGenerator.js
@@ -15,6 +15,26 @@ class GradientGenerator extends React.Component {
     angle: 120
   }
 
+  randomizeGradient = (e) => {
+    e.preventDefault()
+    const hexDigits = ["0", "1", "2", "3" , "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+
+    let firstColor = "#"
+    let secondColor = "#"
+    let angle = Math.floor(Math.random() * 360)
+
+    for(let i=0; i < 6; i++){
+      firstColor += hexDigits[Math.floor(Math.random() * hexDigits.length)]
+      secondColor += hexDigits[Math.floor(Math.random() * hexDigits.length)]
+    }
+
+    this.setState({
+      firstColor,
+      secondColor,
+      angle
+    }, () => Prism.highlightAll())
+  }
+
   render() {
     const { firstColor, secondColor, angle } = this.state;
 
@@ -30,9 +50,9 @@ class GradientGenerator extends React.Component {
           justifyContent="center" 
           alignItems="center" 
           minHeight="100vh">
-          <Card textAlign="center">
+          <Card textAlign="center" maxWidth="100%">
             <CardTitle >Gradient Generator</CardTitle>
-            <Button mb="1.5rem" primary>Generate</Button>
+            <Button onClick={this.randomizeGradient} mb="1.5rem" primary>Generate</Button>
             <pre>
               <code className="language-css">
                 {`background-image: linear-gradient(${angle}deg, ${firstColor} 0%, ${secondColor} 100%);`}
