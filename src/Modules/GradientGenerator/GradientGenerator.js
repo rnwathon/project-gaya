5066a91bfc1b6e903b5b32d2930ff8b7dcd55e1a
rnwathon <rakhmatnashrul@gmail.com>
2020-06-11 09:06:08 +0800

    Higlight prism on component did mount

--- src/Modules/GradientGenerator/GradientGenerator.js
+++ src/Modules/GradientGenerator/GradientGenerator.js
@@ -15,6 +15,10 @@ class GradientGenerator extends React.Component {
     angle: 120
   }
 
+  componentDidMount(){
+    Prism.highlightAll()
+  }
+
   randomizeGradient = (e) => {
     e.preventDefault()
     const hexDigits = ["0", "1", "2", "3" , "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
